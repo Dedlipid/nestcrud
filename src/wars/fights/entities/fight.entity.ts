2be374e0e6eadb9f432b4f9df359ea5634830dbb
Hossein Zamanian <hossein.zamanian@arcadian.ai>
2024-10-22 20:20:08 -0400

    feat(war): add database mapping for war module

--- src/wars/fights/entities/fight.entity.ts
+++ src/wars/fights/entities/fight.entity.ts
@@ -1,1 +1,36 @@
-export class Fight {}
+import {Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {Participant} from "../../entities/participant.entity";
+import {Attack} from "./attack.entity";
+import {Expose} from "class-transformer";
+
+@Entity()
+export class Fight {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    startAt: Date;
+
+    @Column()
+    endAt?: Date
+
+    @ManyToMany(() => Participant)
+    @JoinTable()
+    participants: Participant[]
+
+    @OneToMany(() => Attack, (attack) => attack.fight, {cascade: true})
+    attacks: Attack[]
+
+    @ManyToMany(() => Participant)
+    @JoinTable()
+    winner?: Participant[];
+
+    @ManyToMany(() => Participant)
+    @JoinTable()
+    burnt?: Participant[];
+
+    @Expose()
+    get withdraw() {
+        return !!this.endAt && !this.burnt && !this.winner
+    }
+}
