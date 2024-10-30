23a592aa0446c62e747388b2180ce680ae987190
dedlipid <bamshamaei@gmail.com>
2024-10-30 16:35:41 -0400

    Refactor wars.module.ts to import Hero entity and HeroesModule

--- src/wars/wars.module.ts
+++ src/wars/wars.module.ts
@@ -7,10 +7,16 @@ import { Participant } from './entities/participant.entity';
 import { War } from './entities/war.entity';
 import { ParticipantController } from './participant/participant.controller';
 import { ParticipantService } from './participant/participant.service';
+import { Hero } from 'src/heroes/entities/hero.entity';
+import { HeroesModule } from 'src/heroes/heroes.module';
 
 @Module({
   controllers: [WarsController, ParticipantController],
   providers: [WarsService, ParticipantService],
-  imports: [FightsModule, TypeOrmModule.forFeature([War, Participant])],
+  imports: [
+    FightsModule,
+    TypeOrmModule.forFeature([War, Participant]),
+    HeroesModule,
+  ],
 })
 export class WarsModule {}
