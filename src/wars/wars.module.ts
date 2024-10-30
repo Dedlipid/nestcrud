459839262344c319a66d41c9e482c1a8cb9eda15
dedlipid <bamshamaei@gmail.com>
2024-10-30 14:15:14 -0400

    refactor(wars.module.ts): update imports in WarsModule

--- src/wars/wars.module.ts
+++ src/wars/wars.module.ts
@@ -7,10 +7,16 @@ import { Participant } from './entities/participant.entity';
 import { War } from './entities/war.entity';
 import { ParticipantController } from './participant/participant.controller';
 import { ParticipantService } from './participant/participant.service';
+import { HeroesModule } from 'src/heroes/heroes.module';
+import { League } from 'src/leagues/entities/league.entity';
 
 @Module({
   controllers: [WarsController, ParticipantController],
   providers: [WarsService, ParticipantService],
-  imports: [FightsModule, TypeOrmModule.forFeature([War, Participant])],
+  imports: [
+    FightsModule,
+    TypeOrmModule.forFeature([War, Participant, League]),
+    HeroesModule,
+  ],
 })
 export class WarsModule {}
