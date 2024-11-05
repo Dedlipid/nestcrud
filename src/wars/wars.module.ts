0bba6c7c45607a74e11e1f4fef8055c8b89e9c4b
Bamdad Shamaei <bamshamaei@gmail.com>
2024-11-05 18:19:23 -0500

    Refactor Participant entity, service, and controller (#6)
    * refactor(participant.entity.ts): update primary key types and add join columns
    
    Update the primary key types in the Participant entity to use UUID instead of string. Also, add join columns for the warId and heroId relationships.
    
    * refactor(participant.service.ts): update create and remove methods
    
    * refactor(participant.controller.ts): update create and remove methods
    
    - Update the create method in the ParticipantController to include the warId parameter.
    - Update the remove method in the ParticipantController to include the warId parameter.
    
    * Update participant.service.ts
    
    fix(participant-service): correct type signature for proper dependency injection
    
    * Refactor participant.service.ts to inject repository using @nestjs/typeorm
    
    * Refactor wars.module.ts to import Hero entity and HeroesModule
    
    ---------
    
    Co-authored-by: Hossein Zamanian <129794814+hossein-zamanian@users.noreply.github.com>

--- src/wars/wars.module.ts
+++ src/wars/wars.module.ts
@@ -7,6 +7,9 @@ import { Participant } from './entities/participant.entity';
 import { War } from './entities/war.entity';
 import { ParticipantController } from './participant/participant.controller';
 import { ParticipantService } from './participant/participant.service';
+
+import { Hero } from 'src/heroes/entities/hero.entity';
+import { HeroesModule } from 'src/heroes/heroes.module';
 import { HeroesModule } from 'src/heroes/heroes.module';
 import { League } from 'src/leagues/entities/league.entity';
 import { APP_PIPE } from '@nestjs/core';
