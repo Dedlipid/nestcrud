0477325aff2b895b5679f20f8eef64a3ea808e05
dedlipid <bamshamaei@gmail.com>
2024-11-12 15:57:56 -0500

    refactor(hero): remove unused DTO imports and methods

--- src/heroes/entities/hero.entity.ts
+++ src/heroes/entities/hero.entity.ts
@@ -1,8 +1,5 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { CreateHeroDto } from '../dto/create-hero.dto';
-import { UpdateHeroDto } from '../dto/update-hero.dto';
-import { plainToInstance } from 'class-transformer';
-import { League } from '../../leagues/entities/league.entity';
+import { League } from 'src/leagues/entities/league.entity';
 
 @Entity()
 export class Hero {
@@ -54,20 +51,4 @@ export class Hero {
     nullable: true,
   })
   league?: League;
-
-  static from(createHeroDto: CreateHeroDto): Hero {
-    const hero = plainToInstance(Hero, createHeroDto);
-    if (createHeroDto.lastDamageAt) {
-      hero.lastDamageAt = new Date(createHeroDto.lastDamageAt);
-    }
-    return hero;
-  }
-
-  merge(updateHeroDto: UpdateHeroDto): void {
-    const updatedHero = plainToInstance(Hero, updateHeroDto);
-    Object.assign(this, updatedHero);
-    if (updateHeroDto.lastDamageAt) {
-      this.lastDamageAt = new Date(updateHeroDto.lastDamageAt);
-    }
-  }
 }
