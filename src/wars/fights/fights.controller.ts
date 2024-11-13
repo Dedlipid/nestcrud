47fad54a27bef095a82eb71534cd99cc1754c6d9
dedlipid <bamshamaei@gmail.com>
2024-11-13 14:11:14 -0500

    feat(fight): implement pagination in findAll method and update ID handling to use UUID

--- src/wars/fights/fights.controller.ts
+++ src/wars/fights/fights.controller.ts
@@ -6,10 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  ParseUUIDPipe,
+  Query,
 } from '@nestjs/common';
 import { FightsService } from './fights.service';
 import { CreateFightDto } from './dto/create-fight.dto';
 import { UpdateFightDto } from './dto/update-fight.dto';
+import { UUID } from 'crypto';
+import { PaginationOptions } from 'src/helpers/pagination/interface';
+import { PaginationDto } from 'src/helpers/pagination/pagination-dto';
 
 @Controller('fights')
 export class FightsController {
@@ -21,22 +26,25 @@ export class FightsController {
   }
 
   @Get()
-  findAll() {
-    return this.fightsService.findAll();
+  findAll(@Query() options: PaginationDto) {
+    return this.fightsService.findAll(options);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.fightsService.findOne(+id);
+  findOne(@Param('id', ParseUUIDPipe) id: UUID) {
+    return this.fightsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFightDto: UpdateFightDto) {
-    return this.fightsService.update(+id, updateFightDto);
+  update(
+    @Param('id', ParseUUIDPipe) id: UUID,
+    @Body() updateFightDto: UpdateFightDto,
+  ) {
+    return this.fightsService.update(id, updateFightDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.fightsService.remove(+id);
+  remove(@Param('id', ParseUUIDPipe) id: UUID) {
+    return this.fightsService.remove(id);
   }
 }
--- src/wars/fights/fights.service.ts
+++ src/wars/fights/fights.service.ts
@@ -1,26 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateFightDto } from './dto/create-fight.dto';
 import { UpdateFightDto } from './dto/update-fight.dto';
+import { UUID } from 'crypto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Fight } from './entities/fight.entity';
+import {
+  DEFAULT_TAKE_VALUE,
+  PaginationOptions,
+} from 'src/helpers/pagination/interface';
 
 @Injectable()
 export class FightsService {
+  constructor(
+    @InjectRepository(Fight)
+    private fightRepository: Repository<Fight>,
+  ) {}
+
   create(createFightDto: CreateFightDto) {
-    return 'This action adds a new fight';
+    const fight = this.fightRepository.create(createFightDto);
+    return this.fightRepository.save(fight);
   }
 
-  findAll() {
-    return `This action returns all fights`;
+  findAll(options: PaginationOptions) {
+    const {
+      limit = DEFAULT_TAKE_VALUE,
+      after,
+      before = new Date(),
+      dec = false,
+    } = options;
+    const forceLimit = Math.min(limit, 10);
+
+    const query = this.fightRepository.createQueryBuilder('fight');
+
+    if (after) {
+      query.andWhere('fight.startAt > :after', { after: new Date(after) });
+    }
+
+    if (before) {
+      query.andWhere('fight.startAt < :before', { before: new Date(before) });
+    }
+
+    query.orderBy('fight.startAt', dec ? 'DESC' : 'ASC');
+    query.take(forceLimit);
+
+    return query.getMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} fight`;
+  async findOne(id: UUID) {
+    const fight = await this.fightRepository.findOneBy({ id });
+    if (!fight) throw new NotFoundException(`Fight with ID ${id} not found`);
+    return fight;
   }
 
-  update(id: number, updateFightDto: UpdateFightDto) {
-    return `This action updates a #${id} fight`;
+  async update(id: UUID, updateFightDto: UpdateFightDto) {
+    const entity = await this.fightRepository.findOneBy({ id });
+    if (!entity) throw new NotFoundException(`Fight with ID ${id} not found`);
+    const updateFight = this.fightRepository.merge(entity, updateFightDto);
+    return this.fightRepository.save(updateFight);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} fight`;
+  async remove(id: UUID) {
+    const entity = this.fightRepository.findOneBy({ id });
+    if (!entity) throw new NotFoundException(`Fight with ID ${id} not found`);
+    await this.fightRepository.delete(id);
   }
 }
