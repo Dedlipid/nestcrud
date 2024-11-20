f6193fc38aac48368a7022db682fdc2b666d85d6
dedlipid <bamshamaei@gmail.com>
2024-11-20 15:30:02 -0500

    refactor(pagination/database.ts): simplify date filtering logic in createFindOptions function

--- src/helpers/pagination/database.ts
+++ src/helpers/pagination/database.ts
@@ -1,32 +1,37 @@
-import {FindOptionsWhere, FindOptionsWhereProperty} from 'typeorm/find-options/FindOptionsWhere';
-import {And, FindOptionsOrder, LessThan, MoreThan} from 'typeorm';
-import {FindManyOptions} from 'typeorm/find-options/FindManyOptions';
-import {DEFAULT_TAKE_VALUE, Pageable, PaginationOptions} from './interface';
+import {
+  FindOptionsWhere,
+  FindOptionsWhereProperty,
+} from 'typeorm/find-options/FindOptionsWhere';
+import { Between, FindOptionsOrder, LessThan, MoreThan } from 'typeorm';
+import { FindManyOptions } from 'typeorm/find-options/FindManyOptions';
+import { DEFAULT_TAKE_VALUE, Pageable, PaginationOptions } from './interface';
 
 export function createFindOptions<T extends Pageable>(
-    options: PaginationOptions,
-    advanceOptions?: FindOptionsWhere<T>,
+  options: PaginationOptions,
+  advanceOptions: FindOptionsWhere<T> = {},
 ): FindManyOptions<T> {
-    const whereOption: FindOptionsWhere<T> = {};
+  const whereOption: FindOptionsWhere<T> = {};
 
-    if (options.after && options.before) {
-        whereOption.createdAt = And(MoreThan(new Date(options.after)), LessThan(new Date(options.before))) as FindOptionsWhereProperty<NonNullable<T['createdAt']>>
-    }
+  if (options.after && options.before) {
+    whereOption.createdAt = Between(
+      new Date(options.after),
+      new Date(options.before),
+    ) as FindOptionsWhereProperty<NonNullable<T['createdAt']>>;
+  } else if (options.before) {
+    whereOption.createdAt = LessThan(
+      new Date(options.before),
+    ) as FindOptionsWhereProperty<NonNullable<T['createdAt']>>;
+  } else if (options.after) {
+    whereOption.createdAt = MoreThan(
+      new Date(options.after),
+    ) as FindOptionsWhereProperty<NonNullable<T['createdAt']>>;
+  }
 
-    if (options.before) {
-        whereOption.createdAt = LessThan(new Date(options.before)) as FindOptionsWhereProperty<NonNullable<T['createdAt']>>
-    }
-
-    if (options.after) {
-        whereOption.createdAt = MoreThan(new Date(options.after)) as FindOptionsWhereProperty<NonNullable<T['createdAt']>>
-    }
-
-
-    return {
-        order: {
-            createdAt: options.dec ? 'desc' : 'asc',
-        } as FindOptionsOrder<T>,
-        take: options.limit || DEFAULT_TAKE_VALUE,
-        where: Object.assign(advanceOptions || {}, whereOption)
-    };
+  return {
+    order: {
+      createdAt: options.dec ? 'desc' : 'asc',
+    } as FindOptionsOrder<T>,
+    take: options.limit || DEFAULT_TAKE_VALUE,
+    where: { ...advanceOptions, ...whereOption },
+  };
 }
